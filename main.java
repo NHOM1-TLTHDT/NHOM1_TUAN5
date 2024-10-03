3abace0333d436ad1854ac70405af6c8bffa617a
HuyHoangIT <huyhoangwf@gmail.com>
2024-10-03 20:36:44 +0700

    huy_hoang - bai7

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
-class Student {
+class Student{
     private String studentId;
     private String name;
     private double averageScore;
